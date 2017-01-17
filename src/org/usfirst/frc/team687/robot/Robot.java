9e2b34c12467eaf38fcc80cbd7203a25b486eaed
Michael Iovine <mike.iovine7@gmail.com>
2017-01-16 19:26:17 -0800

    initial commit

---
+++ src/org/usfirst/frc/team687/robot/Robot.java
@@ -0,0 +1,48 @@
+
+package org.usfirst.frc.team687.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+
+public class Robot extends IterativeRobot {
+
+	@Override
+	public void robotInit() {
+
+	}
+
+	@Override
+	public void disabledInit() {
+
+	}
+
+	@Override
+	public void disabledPeriodic() {
+		
+	}
+
+	@Override
+	public void autonomousInit() {
+
+	}
+
+	@Override
+	public void autonomousPeriodic() {
+		
+	}
+
+	@Override
+	public void teleopInit() {
+		
+	}
+
+	@Override
+	public void teleopPeriodic() {
+		
+	}
+
+	@Override
+	public void testPeriodic() {
+		
+	}
+}
