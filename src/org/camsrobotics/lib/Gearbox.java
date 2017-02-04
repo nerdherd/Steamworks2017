f9c20cc619c153c85c20aa7b859e624784783635
Michael Iovine <mike.iovine7@gmail.com>
2017-02-04 01:40:54 -0800

    more gearbox

--- src/org/camsrobotics/lib/Gearbox.java
+++ src/org/camsrobotics/lib/Gearbox.java
@@ -35,9 +35,7 @@ public class Gearbox {
 		m_encoderTalon = encoderTalon;
 		m_talon1 = talon1;
 		m_talon2 = talon2;
-		
-		m_talon1.setFeedbackDevice(FeedbackDevice.CtreMagEncoder_Relative);
-		m_talon2.setFeedbackDevice(FeedbackDevice.CtreMagEncoder_Relative);
+	
 		m_encoderTalon.setFeedbackDevice(FeedbackDevice.CtreMagEncoder_Relative);
 	}
 	
@@ -70,7 +68,34 @@ public class Gearbox {
 	}
 	
 	public void exectuteProfile(double maxVelocity, double maxAcceleration, double revolutions) {
-		//TODO
+		m_encoderTalon.changeControlMode(TalonControlMode.MotionProfile);
+		m_talon1.changeControlMode(TalonControlMode.Follower);
+		m_talon2.changeControlMode(TalonControlMode.Follower);
+		
+		resetProfile();
+		
+		m_profile = new MotionProfile(maxVelocity, maxAcceleration, revolutions);
+		m_points = m_profile.getPoints(kIntervalSize);
+		m_duration = m_points[0][0];
+		
+		CANTalon.TrajectoryPoint point = new CANTalon.TrajectoryPoint();
+		for (int i=0; i < kIntervalSize; i++) {
+			point.velocity = m_points[0][i];
+			point.timeDurMs = (int) m_points[i][0];
+			point.velocityOnly = true;
+			
+			m_encoderTalon.pushMotionProfileTrajectory(point);
+		}
+		m_encoderTalon.set(CANTalon.SetValueMotionProfile.Enable.value);
+		m_talon1.set(m_encoderTalon.getDeviceID());
+		m_talon2.set(m_encoderTalon.getDeviceID());
+		
+		m_encoderTalon.changeMotionControlFramePeriod((int) m_duration);
+		m_notifier.startPeriodic((int) m_duration/1000);
+		
 	}
 	
+	private void resetProfile() {
+		m_encoderTalon.clearMotionProfileTrajectories();
+	}
 }
