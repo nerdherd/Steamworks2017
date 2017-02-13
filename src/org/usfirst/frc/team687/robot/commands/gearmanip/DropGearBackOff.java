536767d59a4e50d13a830eee9ff5f6fedbfe2d82
Wesley Soo-Hoo <wesoohoo@gmail.com>
2017-02-13 03:15:49 -0800

    pause a little

--- src/org/usfirst/frc/team687/robot/commands/gearmanip/DropGearBackOff.java
+++ src/org/usfirst/frc/team687/robot/commands/gearmanip/DropGearBackOff.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team687.robot.commands.gearmanip;
 
+import org.usfirst.frc.team687.robot.commands.TimeoutCommand;
 import org.usfirst.frc.team687.robot.commands.drive.DriveTime;
 import org.usfirst.frc.team687.robot.commands.drive.ShiftDown;
 import org.usfirst.frc.team687.robot.commands.drive.ShiftUp;
@@ -12,6 +13,7 @@ public class DropGearBackOff extends CommandGroup{
 	public DropGearBackOff() {
 		addSequential(new ShiftDown());
 		addParallel(new GearManipSetTimer(GearManipulationConstants.kGearManipDownPos, 2));
+		addSequential(new TimeoutCommand(.25));
 		addSequential(new DriveTime(-0.4, -0.4, 2));
 		addParallel(new GearManipSetNoTimer(GearManipulationConstants.kGearManipUpPos));
 		addParallel(new ShiftUp());
